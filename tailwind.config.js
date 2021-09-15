51816eee47a99a7081d312e02b2f9a41601bf0fd
Tim.Hsu <tim.hsu@tpisoftware.com>
2021-09-15 17:45:47 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  mode: "jit",
   purge: {
-    enabled: true,
-    content: ["./src/**/*.html", "./src/**/*.scss"],
+    // enabled: true,
+    enabled: process.env.TAILWIND_MODE === "build",
+    content: ["./src/app/**/*.html", "./src/**/*.scss"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
